9102c702fc2eba0c203756b7ed0a6ad3889cb239
Raphael <raaphael.lima44@gmail.com>
2024-03-06 18:29:08 -0300

    test

---
+++ 2dshapes/src/circulo.java
@@ -0,0 +1,29 @@
+
+
+public class circulo {
+    public void desenho(){
+    
+      int x = 15;
+      int y = 15;
+  
+      int[][] plano = new int[x][y];
+  
+      int i, j;
+      for(i=0;i<x;i++){
+          for(j=0;j<y;j++){
+              plano[i][j] = 1;
+          }
+      }
+  
+      //
+  
+  
+      for(i=0;i<x;i++){
+        for(j=0;j<y;j++){
+            System.out.print(plano[i][j] +" ");
+        }
+        System.out.print("\n");
+  
+      }
+    }
+  }
--- 2dshapes/src/triangulo.java
+++ 2dshapes/src/triangulo.java
@@ -1,32 +1,32 @@
 
 public class triangulo {
-    public void desenho(){
-  
-      int x = 15;
-      int y = 15;
-  
-      int[][] plano = new int[x][y];
-  
-      int i, j;
-      for(i=0;i<x;i++){
-          for(j=0;j<y;j++){
-              plano[i][j] = 1;
-          }
-      }
-  
-      //
-      for(i = 0; i<15; i++){
-        for(j = i; j<15-i; j++){
-          plano[i][j] = 8;
-        }
-      }
-  
-      for(i=0;i<x;i++){
+  public void desenho(){
+
+    int x = 15;
+    int y = 15;
+
+    int[][] plano = new int[x][y];
+
+    int i, j;
+    for(i=0;i<x;i++){
         for(j=0;j<y;j++){
-            System.out.print(plano[i][j] +" ");
+            plano[i][j] = 1;
         }
-        System.out.print("\n");
-  
+    }
+
+    //
+    for(i = 0; i<15; i++){
+      for(j = i; j<15-i; j++){
+        plano[i][j] = 8;
       }
     }
+
+    for(i=x-1;i>=0;i--){
+      for(j=0;j<y;j++){
+          System.out.print(plano[i][j] +" ");
+      }
+      System.out.print("\n");
+
+    }
   }
+}
